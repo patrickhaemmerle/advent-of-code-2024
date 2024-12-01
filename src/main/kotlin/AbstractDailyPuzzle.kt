70eaff213655a8753a8e9ee3dd13539dfcc5bc1d
Patrick Hämmerle <patrick.haemmerle@gmail.com>
2024-12-01 07:53:13 +0100

    Remove unused code

--- src/main/kotlin/AbstractDailyPuzzle.kt
+++ src/main/kotlin/AbstractDailyPuzzle.kt
@@ -4,10 +4,6 @@ abstract class AbstractDailyPuzzle {
 
     fun run() {
         val inputDir = File("input")
-        val outputDir = File("output")
-
-        if (outputDir.exists()) outputDir.deleteRecursively()
-        outputDir.mkdirs()
 
         val input1 = File("${inputDir.path}/input1.txt")
         val input2 = File("${inputDir.path}/input2.txt")
