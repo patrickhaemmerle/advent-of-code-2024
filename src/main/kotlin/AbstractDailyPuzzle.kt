38f045c8d1ab967729ca2f6bf0c343b6ae044dab
Patrick Hämmerle <patrick.haemmerle@gmail.com>
2024-12-01 07:59:08 +0100

    Pretty print the results

--- src/main/kotlin/AbstractDailyPuzzle.kt
+++ src/main/kotlin/AbstractDailyPuzzle.kt
@@ -3,6 +3,9 @@ import java.io.File
 abstract class AbstractDailyPuzzle {
 
     fun run() {
+        println("Runnin AoC for ${this.javaClass.name}")
+        println("-".repeat(80))
+
         val inputDir = File("input")
 
         val input1 = File("${inputDir.path}/input1.txt")
@@ -10,6 +13,7 @@ abstract class AbstractDailyPuzzle {
 
         println("Solution to part1 is: ${part1(input1.readLines())}")
         println("Solution to part2 is: ${part2(input2.readLines())}")
+        println("-".repeat(80))
     }
 
     abstract fun part1(input: List<String>): String
