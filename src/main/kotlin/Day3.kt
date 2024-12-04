19318e9a0c3cf22283a74e5625af8e59de94fff2
Patrick Hämmerle <patrick.haemmerle@gmail.com>
2024-12-04 05:52:55 +0100

    Day3

---
+++ src/main/kotlin/Day3.kt
@@ -0,0 +1,37 @@
+fun main() = Day3().run()
+
+class Day3 : AbstractDailyPuzzle() {
+
+    override fun part1(input: List<String>): String {
+        val result = input.map { str ->
+            "mul\\((\\d+),(\\d+)\\)".toRegex()
+                .findAll(str)
+                .map {
+                    it.groups[1]!!.value.toLong() * it.groups[2]!!.value.toLong()
+                }.toList().sum()
+        }.sum()
+        return result.toString()
+    }
+
+    override fun part2(input: List<String>): String {
+        var multiply = true
+        val result = input.map { str ->
+            "mul\\((\\d+),(\\d+)\\)|do\\(\\)|don't\\(\\)".toRegex()
+                .findAll(str)
+                .mapNotNull {
+                    if (it.groups[0]!!.value == "do()") {
+                        multiply = true
+                        null
+                    } else if (it.groups[0]!!.value == "don't()") {
+                        multiply = false
+                        null
+                    } else if (multiply) {
+                        it.groups[1]!!.value.toLong() * it.groups[2]!!.value.toLong()
+                    } else {
+                        null
+                    }
+                }.toList().sum()
+        }.sum()
+        return result.toString()
+    }
+}
