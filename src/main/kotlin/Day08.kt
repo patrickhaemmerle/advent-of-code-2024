310766244decf7997be16a47e62d1f887355f35e
Patrick Hämmerle <patrick.haemmerle@gmail.com>
2024-12-08 07:31:38 +0100

    Day 8 - Refactored

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -4,48 +4,61 @@ class Day08 : AbstractDailyPuzzle() {
 
     override fun part1(input: List<String>): String {
         val antennasByFrequency = readAntennaLocations(input).groupBy { it.frequency }
-        val antinodes = antennasByFrequency.flatMap { (_, antennas) ->
-            val antinodesForFrequency = mutableListOf<Pair<Int, Int>>()
-            for (i in antennas.indices) {
-                for (j in i + 1 until antennas.size) {
-                    val distance = Pair(antennas[i].x - antennas[j].x, antennas[i].y - antennas[j].y)
-                    antinodesForFrequency.add(Pair(antennas[i].x + distance.first, antennas[i].y + distance.second))
-                    antinodesForFrequency.add(Pair(antennas[j].x - distance.first, antennas[j].y - distance.second))
-                }
-            }
-            antinodesForFrequency
-        }.filter {
-            !(it.first < 0 || it.second < 0 || it.first >= input.first().length || it.second >= input.size)
+        val mapDimensionX = input.first().length
+        val mapDimensionY = input.size
+
+        val antiNodes = antennasByFrequency.flatMap { (_, antennas) ->
+            antennas.mapIndexed { i, firstAntenna ->
+                antennas.subList(i + 1, antennas.size).map { secondAntenna ->
+                    val xDistance = firstAntenna.x - secondAntenna.x
+                    val yDistance = firstAntenna.y - secondAntenna.y
+                    listOf(
+                        AntiNode(firstAntenna.x + xDistance, firstAntenna.y + yDistance),
+                        AntiNode(secondAntenna.x - xDistance, secondAntenna.y - yDistance),
+                    ).filter { coordinateIsWithinMap(it.x, it.y, mapDimensionX, mapDimensionY) }
+                }.flatten()
+            }.flatten()
         }.toSet().toList()
-        return antinodes.count().toString()
+        return antiNodes.count().toString()
     }
 
     override fun part2(input: List<String>): String {
         val antennasByFrequency = readAntennaLocations(input).groupBy { it.frequency }
-        val antinodes = antennasByFrequency.flatMap { (_, antennas) ->
-            val antinodesForFrequency = mutableListOf<Pair<Int, Int>>()
-            for (i in antennas.indices) {
-                antinodesForFrequency.add(Pair(antennas[i].x, antennas[i].y))
-                for (j in i + 1 until antennas.size) {
-                    val step = Pair(antennas[i].x - antennas[j].x, antennas[i].y - antennas[j].y)
-                    var distance = step
-                    while (true) {
-                        val newAntinodes = listOf(
-                            Pair(antennas[i].x + distance.first, antennas[i].y + distance.second),
-                            Pair(antennas[j].x - distance.first, antennas[j].y - distance.second)
-                        ).filter {
-                            !(it.first < 0 || it.second < 0 || it.first >= input.first().length || it.second >= input.size)
-                        }
-                        if (newAntinodes.isEmpty()) break
-                        antinodesForFrequency.addAll(newAntinodes)
-                        distance = Pair(step.first + distance.first, step.second + distance.second)
-                    }
-                }
-            }
-            antinodesForFrequency
+        val mapDimensionX = input.first().length
+        val mapDimensionY = input.size
+
+        val antiNodes = antennasByFrequency.flatMap { (_, antennas) ->
+            antennas.mapIndexed { i, firstAntenna ->
+                listOf(AntiNode(firstAntenna.x, firstAntenna.y)) +
+                        antennas.subList(i + 1, antennas.size)
+                            .map { secondAntenna ->
+                                val xStep = firstAntenna.x - secondAntenna.x
+                                val yStep = firstAntenna.y - secondAntenna.y
+                                var xDistance = xStep
+                                var yDistance = yStep
+                                val antiNodesForThisPairOfAntennas = mutableListOf<AntiNode>()
+                                while (true) {
+                                    val newAntinodes = listOf(
+                                        AntiNode(firstAntenna.x + xDistance, firstAntenna.y + yDistance),
+                                        AntiNode(secondAntenna.x - xDistance, secondAntenna.y - yDistance),
+                                    ).filter { coordinateIsWithinMap(it.x, it.y, mapDimensionX, mapDimensionY) }
+
+                                    if (newAntinodes.isEmpty()) break
+
+                                    antiNodesForThisPairOfAntennas.addAll(newAntinodes)
+                                    xDistance += xStep
+                                    yDistance += yStep
+                                }
+                                antiNodesForThisPairOfAntennas
+
+                            }.flatten()
+            }.flatten()
         }.toSet().toList()
-        return antinodes.count().toString()
+        return antiNodes.count().toString()
     }
+
+    private fun coordinateIsWithinMap(x: Int, y: Int, xDimension: Int, yDimension: Int) =
+        x >= 0 && y >= 0 && x < xDimension && y < yDimension
 
     private fun readAntennaLocations(input: List<String>): List<Antenna> = input
         .mapIndexed { y, line ->
@@ -59,7 +72,12 @@ class Day08 : AbstractDailyPuzzle() {
     data class Antenna(
         val x: Int,
         val y: Int,
-        val frequency: Char
+        val frequency: Char,
+    )
+
+    data class AntiNode(
+        val x: Int,
+        val y: Int,
     )
 
 }
