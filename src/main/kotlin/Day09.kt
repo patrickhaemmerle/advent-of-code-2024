c2200c54564f0e6797d5a09a47f9b54adebdbd50
Patrick Hämmerle <patrick.haemmerle@gmail.com>
2024-12-10 05:53:46 +0100

    Day 9

--- src/main/kotlin/Day09.kt
+++ src/main/kotlin/Day09.kt
@@ -3,11 +3,116 @@ fun main() = Day09().run()
 class Day09 : AbstractDailyPuzzle() {
 
     override fun part1(input: List<String>): String {
-        return "-"
+        val diskMap = input.first().toCharArray()
+        val decompressedDisk = ArrayDeque<DiskSlot>()
+
+        diskMap.forEachIndexed() { i, value ->
+            if (i % 2 == 0) {
+                decompressedDisk.addLast(File(value.digitToInt(), i / 2))
+            } else {
+                decompressedDisk.addLast(EmptySpace(value.digitToInt()))
+            }
+        }
+
+        val compressedDisk = mutableListOf<DiskSlot>()
+
+        while (decompressedDisk.isNotEmpty()) {
+            val current = decompressedDisk.removeFirst()
+            if (current is File) {
+                compressedDisk.add(current)
+            } else {
+                while (decompressedDisk.isNotEmpty() && decompressedDisk.last() is EmptySpace) decompressedDisk.removeLast()
+                if (decompressedDisk.isNotEmpty()) {
+                    val last = decompressedDisk.removeLast() as File
+                    val freeSpace = current.length
+                    if (last.length <= freeSpace) {
+                        compressedDisk.add(last)
+                        if (last.length < freeSpace) {
+                            decompressedDisk.addFirst(EmptySpace(freeSpace - last.length))
+                        }
+                    } else {
+                        compressedDisk.add(File(freeSpace, last.id))
+                        decompressedDisk.addLast(File(last.length - freeSpace, last.id))
+                    }
+                }
+            }
+        }
+
+        var pos = 0
+        var sum = 0L
+        compressedDisk.forEach {
+            if (it is File) {
+                for (i in 0 until it.length) {
+                    sum += (pos * it.id)
+                    pos++
+                }
+            }
+        }
+
+        return sum.toString()
     }
 
     override fun part2(input: List<String>): String {
-        return "-"
+        val diskMap = input.first().toCharArray()
+        val disk = ArrayDeque<DiskSlot>()
+
+        diskMap.forEachIndexed() { i, value ->
+            if (i % 2 == 0) {
+                disk.addLast(File(value.digitToInt(), i / 2))
+            } else {
+                disk.addLast(EmptySpace(value.digitToInt()))
+            }
+        }
+
+        var attemptToMoveIndex = disk.size - 1
+        while (attemptToMoveIndex > 0) {
+            if (disk[attemptToMoveIndex] is File) {
+                for (i in 1 until attemptToMoveIndex) {
+                    if (disk[i] is EmptySpace && disk[i].length >= disk[attemptToMoveIndex].length) {
+                        val emptySpace = EmptySpace(disk[i].length - disk[attemptToMoveIndex].length)
+                        val move = disk[attemptToMoveIndex]
+                        disk[attemptToMoveIndex] = EmptySpace(move.length)
+                        disk[i] = emptySpace
+                        disk.add(i, move)
+                        break
+                    }
+                }
+            }
+            attemptToMoveIndex--
+        }
+
+        var pos = 0
+        var sum = 0L
+        disk
+            .forEach {
+                if (it is File) {
+                    for (i in 0 until it.length) {
+                        sum += (pos * it.id)
+                        pos++
+                    }
+                } else {
+                    pos += it.length
+                }
+            }
+
+        return sum.toString()
     }
 
+}
+
+interface DiskSlot {
+    val length: Int
+}
+
+data class EmptySpace(
+    override val length: Int
+) : DiskSlot {
+    override fun toString(): String = ".".repeat(length)
+}
+
+data class File(
+    override val length: Int,
+    val id: Int
+) : DiskSlot{
+    override fun toString(): String = id.toString().repeat(length)
 }
