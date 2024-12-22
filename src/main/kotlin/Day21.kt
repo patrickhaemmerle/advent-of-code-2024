373db0bef85eac6c8b9f1e53e98bd93f6ec5900a
Patrick Hämmerle <patrick.haemmerle@gmail.com>
2024-12-22 16:42:40 +0100

    Day21 - Part 1

--- src/main/kotlin/Day21.kt
+++ src/main/kotlin/Day21.kt
@@ -1,6 +1,7 @@
 import util.CharMatrixCellTransformer
+import util.Direction
 import util.Matrix
-import kotlin.math.absoluteValue
+import util.MatrixCell
 
 fun main() = Day21().run()
 
@@ -17,99 +18,134 @@ class Day21 : AbstractDailyPuzzle() {
         CharMatrixCellTransformer()
     )
 
-    val resolveCodeCache = mutableMapOf<Pair<String, Matrix<Char>>, List<String>>()
-    val resolveMultilevelCodeCache = mutableMapOf<Triple<String, List<Matrix<Char>>, Int>, List<String>>()
+    val directionToCommand = mapOf(
+        Direction.NORTH to '^',
+        Direction.WEST to '<',
+        Direction.SOUTH to 'v',
+        Direction.EAST to '>',
+    )
+
+    val numericKeyMap = findAllShortestPathsForKeyPad(numericKeyPad)
+    val directionalKeyMap = findAllShortestPathsForKeyPad(directionalKeyPad)
 
     override fun part1(input: List<String>): String {
-        val cascade = listOf(directionalKeyPad, directionalKeyPad, numericKeyPad)
-        resolveMutlilevelCode("029A", cascade, 0)
-            .filter { it == "<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A" }
-            .forEach { println(it) }
-        return "-"
+        val cascade = listOf(directionalKeyMap, directionalKeyMap, numericKeyMap)
+        return input.sumOf { findCostForPathPressingEveryKey(it, cascade) * it.dropLast(1).toLong() }.toString()
     }
 
     override fun part2(input: List<String>): String {
         return "-"
     }
 
-    private fun resolveMutlilevelCode(code: String, keyPads: List<Matrix<Char>>, depth: Int): List<String> {
-        if (depth >= keyPads.size - 1) return resolveCode(code, keyPads[depth])
+    private fun findCostForPathPressingEveryKey(path: String, cascade: List<Map<Pair<Char, Char>, Set<String>>>): Int {
+        return ("A$path").zipWithNext { a, b -> findCostForTransitionAndPress(a, b, cascade) }.sum()
+    }
 
-        val cacheKey = Triple(code, keyPads, depth)
-        if (cacheKey in resolveMultilevelCodeCache) return resolveMultilevelCodeCache[cacheKey]!!
-
-        val result = resolveMutlilevelCode(code, keyPads, depth + 1).flatMap {
-            resolveCode(it, keyPads[depth])
-        }.groupBy { it.length }.minByOrNull { it.key }?.value ?: emptyList()
-
-        resolveMultilevelCodeCache[cacheKey] = result
+    private fun findCostForTransitionAndPress(
+        from: Char,
+        to: Char,
+        cascade: List<Map<Pair<Char, Char>, Set<String>>>
+    ): Int {
+        val result = findPathsForTransitionAndPressCascaded("$from$to", cascade, 0).minBy { it.length }.length
         return result
     }
 
-    private fun resolveCode(code: String, keyPad: Matrix<Char>): List<String> {
-        if (code.length == 1) return findAllShortestSequencesToMoveAndPress('A', code[0], keyPad).map { it + "A" }
-        val cacheKey = Pair(code, keyPad)
-        if (cacheKey in resolveCodeCache) return resolveCodeCache[cacheKey]!!
+    private fun findPathsForTransitionAndPressCascaded(
+        code: String,
+        cascade: List<Map<Pair<Char, Char>, Set<String>>>,
+        depth: Int,
+    ): Set<String> {
+        if (depth >= cascade.size - 1) return findPathsForTransitionAndPress(code, cascade[depth])
 
-        val untilHere = resolveCode(code.substring(0, code.length - 1), keyPad)
-        val result = untilHere.flatMap { u ->
-            findAllShortestSequencesToMoveAndPress(
-                code[code.length - 2],
-                code[code.length - 1],
-                keyPad
-            ).map { u + it + "A" }
-        }.groupBy { it.length }.minBy { it.key }.value
-        resolveCodeCache[cacheKey] = result
+        val result = findPathsForTransitionAndPressCascaded(code, cascade, depth + 1).flatMap {
+            findPathsPressingEveryKey("A" + it, cascade[depth])
+        }.groupBy { it.length }.minBy { it.key }.value.toSet()
+
         return result
     }
 
-    private fun precalcAllShortestPaths(keyPad: Matrix<Char>): Map<Pair<Char, Char>, List<String>> {
-        val cells = keyPad.cells().filter { it.value != '.' }
-        val map = mutableMapOf<Pair<Char, Char>, List<String>>()
-        for (i in 0 until cells.size) {
-            for (j in 0 until cells.size) {
-                map[Pair(cells[i].value, cells[j].value)] =
-                    findAllShortestSequencesToMoveAndPress(cells[i].value, cells[j].value, keyPad)
+    private fun findPathsPressingEveryKey(
+        code: String,
+        pathMap: Map<Pair<Char, Char>, Set<String>>,
+    ): Set<String> {
+        if (code.length == 2) return pathMap[code[0] to code[1]]!!.map { it + "A" }.toSet()
+
+        val result = findPathsPressingEveryKey(code.substring(0, code.length - 1), pathMap).flatMap { previous ->
+            pathMap[code[code.length - 2] to code[code.length - 1]]!!.map { previous + it + "A" }
+        }.groupBy { it.length }.minBy { it.key }.value.toSet()
+
+        return result
+    }
+
+    private fun findPathsForTransitionAndPress(
+        code: String,
+        pathMap: Map<Pair<Char, Char>, Set<String>>,
+    ): Set<String> {
+        if (code.length == 2) return pathMap[code[0] to code[1]]!!.map { it + "A" }.toSet()
+
+        val result =
+            findPathsForTransitionAndPress(code.substring(0, code.length - 1), pathMap).flatMap { previous ->
+                pathMap[code[code.length - 2] to code[code.length - 1]]!!.map { previous + it }
+            }.groupBy { it.length }.minBy { it.key }.value.toSet()
+
+        return result
+    }
+
+    private fun findAllShortestPathsForKeyPad(keyPad: Matrix<Char>): MutableMap<Pair<Char, Char>, Set<String>> {
+        val keys = keyPad.cells().filter { it.value != '.' }
+        val map = mutableMapOf<Pair<Char, Char>, Set<String>>()
+        for (key1 in keys) {
+            for (key2 in keys) {
+                map[key1.value to key2.value] = findShortestPaths(key1, key2, keyPad)
             }
         }
         return map
     }
 
-    private fun findAllShortestSequencesToMoveAndPress(from: Char, to: Char, keyPad: Matrix<Char>): List<String> {
-        val source = keyPad.allCellsWithValue(from).single()
-        val target = keyPad.allCellsWithValue(to).single()
-        val deadKey = keyPad.allCellsWithValue('.').single()
+    private fun findShortestPaths(from: MatrixCell<Char>, to: MatrixCell<Char>, keyPad: Matrix<Char>): Set<String> {
+        val visited = mutableMapOf<MatrixCell<Char>, BackTracking>()
+        val toVisit = ArrayDeque<Pair<MatrixCell<Char>, BackTracking>>()
 
-        val vertical = if (target.i > source.i) "v" else "^"
-        val horizontal = if (target.j > source.j) ">" else "<"
+        toVisit.add(
+            Pair(
+                from,
+                BackTracking(0, mutableSetOf(""))
+            )
+        )
 
-        val iDiff = (target.i - source.i).absoluteValue
-        val jDiff = (target.j - source.j).absoluteValue
-
-        val iDead = (deadKey.i - source.i).absoluteValue
-        val jDead = (deadKey.j - source.j).absoluteValue
-
-        fun findCombos(i: Int, j: Int, iDead: Int = -1, jDead: Int = -1): List<String> {
-            if (i == 0 && j == 0) return listOf("")
-            if (iDead == 0 && jDead == 0) return emptyList()
-            val results = mutableListOf<String>()
-            if (i != 0) {
-                findCombos(i - 1, j, iDead - 1, jDead).map { "$vertical$it" }.forEach { results.add(it) }
+        while (toVisit.isNotEmpty()) {
+            val current = toVisit.removeFirst()
+            if (current.first in visited && current.second.cost > visited[current.first]!!.cost) {
+                continue
             }
-            if (j != 0) {
-                findCombos(i, j - 1, iDead, jDead - 1).map { "$horizontal$it" }.forEach { results.add(it) }
+            if (current.first != to) {
+                directionToCommand.forEach { direction ->
+                    keyPad.allAdjacentCells(current.first, setOf(direction.key)).filter { it.value != '.' }
+                        .forEach {
+                            toVisit.add(
+                                Pair(
+                                    it,
+                                    BackTracking(
+                                        current.second.cost + 1,
+                                        current.second.paths.map { it + directionToCommand[direction.key] }
+                                            .toMutableSet()
+                                    )
+                                )
+                            )
+                        }
+                }
             }
-            return results
+            if (current.first in visited && current.second.cost == visited[current.first]!!.cost) {
+                visited[current.first]!!.paths.addAll(current.second.paths)
+            } else {
+                visited[current.first] = current.second
+            }
         }
-
-        val result = if (source.i == deadKey.i && target.j == deadKey.j && iDiff > 0) {
-            findCombos(iDiff, jDiff, iDead, jDead)
-        } else if (source.j == deadKey.j && target.i == deadKey.i) {
-            findCombos(iDiff, jDiff, iDead, jDead)
-        } else {
-            findCombos(iDiff, jDiff)
-        }
-
-        return result
+        return visited[to]?.paths ?: emptySet()
     }
 }
+
+private data class BackTracking(
+    val cost: Int,
+    val paths: MutableSet<String>
+)
