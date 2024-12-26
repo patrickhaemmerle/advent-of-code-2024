9fdd6c90305dc472fc7439ace5da89a453e9e74f
Patrick Hämmerle <patrick.haemmerle@gmail.com>
2024-12-26 22:46:26 +0100

    Day24

--- src/main/kotlin/Day24.kt
+++ src/main/kotlin/Day24.kt
@@ -1,3 +1,5 @@
+import Day24.Operator.*
+
 fun main() = Day24().run()
 
 class Day24 : AbstractDailyPuzzle() {
@@ -9,74 +11,48 @@ class Day24 : AbstractDailyPuzzle() {
     }
 
     override fun part2(input: List<String>): String {
+        // This code validates the addition machine, errors are reported and fixes need to be made manually
+        if (input.size == 0) return "-"
         val device = readInput(input)
-        val actualResult = device.solve()
-        val expectedResult = evaluateExpectedResult(device).padStart(actualResult.length, '0')
+        var remainder = "hfm"
+        for (i in 1 until 45) {
+            println("testing $i")
 
-//        for (i in 0 until expectedResult)
-//        val candidates = device.gates
-//            .filter { it.key.startsWith("z") }
-//            .map { findCandidatePairsToSwap(it.key, device) }
-//            .toList().reversed()
-//            .mapIndexed { i, it ->
-//                if (expectedResult[i] == actualResult[i]) null else it
-//            }.filterNotNull()
-
-        return "-"
-    }
-
-    private fun findCandidatePairsToSwap(endPoint: String, device: Device): Set<Pair<String, String>> {
-        val candidates = findUpstreamCandidatesToSwap(endPoint, device).toList()
-        val candidatePairs = mutableSetOf<Pair<String, String>>()
-        for (i in candidates.indices) {
-            for (j in i + 1 until candidates.size) {
-                if (device.solveForValueOnWire(candidates[i]) != device.solveForValueOnWire(candidates[j])) {
-                    if (candidates[i] < candidates[j]) candidatePairs.add(candidates[i] to candidates[j])
-                    else candidatePairs.add(candidates[j] to candidates[i])
+            // AND and XOR
+            val level1 = device.gates
+                .filter {
+                    (
+                            it.value.first == "x" + (i.toString().padStart(2, '0'))
+                                    || it.value.first == "y" + (i.toString().padStart(2, '0'))
+                            ) && (
+                            it.value.second.endsWith(i.toString().padStart(2, '0'))
+                                    || it.value.second.endsWith(i.toString().padStart(2, '0'))
+                            ) && it.value.first != it.value.second
                 }
-            }
+            check(level1.size == 2)
+            check(level1.filter { it.value.third == XOR }.size == 1)
+            check(level1.filter { it.value.third == AND }.size == 1)
+            val level1Xor = level1.filter { it.value.third == XOR }.map { it.key }.single()
+            val level1And = level1.filter { it.value.third == AND }.map { it.key }.single()
+
+            // Remainder level 1
+            val remainderLevel1Xor =
+                device.gates.filter { (it.value.first == level1Xor && it.value.second == remainder) || (it.value.first == remainder && it.value.second == level1Xor) }
+                    .filter { it.value.third == XOR }
+                    .toList().single()
+            check (remainderLevel1Xor.first == "z${i.toString().padStart(2, '0')}")
+
+            val remainderLevel1And =
+                device.gates.filter { (it.value.first == level1Xor && it.value.second == remainder) || (it.value.first == remainder && it.value.second == level1Xor) }
+                    .filter { it.value.third == AND }
+                    .toList().single()
+            val remainderLevel2Or =
+                device.gates.filter { (it.value.first == level1And && it.value.second == remainderLevel1And.first) || (it.value.first == remainderLevel1And.first && it.value.second == level1And) }
+                    .filter { it.value.third == OR }
+                    .toList().single()
+            remainder = remainderLevel2Or.first
         }
-        val otherEndpoints = device.gates.keys
-            .filter { it.startsWith("z") }
-            .filter { device.solveForValueOnWire(it) != device.solveForValueOnWire(endPoint) }
-            .map { endPoint to it }
-        return candidatePairs + otherEndpoints
-    }
-
-    private fun findUpstreamCandidatesToSwap(start: String, device: Device): Set<String> {
-        if (!device.gates.contains(start)) return emptySet()
-        return findUpstreamCandidatesToSwap(
-            device.gates[start]!!.first,
-            device
-        ) + findUpstreamCandidatesToSwap(
-            device.gates[start]!!.second,
-            device
-        ) + setOf(start)
-    }
-
-    private fun performHealthCheck(device: Device): Boolean {
-        val expectedResult = evaluateExpectedResult(device)
-        val actualResult = device.solve()
-        return actualResult == expectedResult
-    }
-
-    private fun evaluateExpectedResult(device: Device): String {
-        val summand1 = device.input
-            .filter { it.key.startsWith("x") }
-            .map { if (it.value) "1" else "0" }
-            .reversed()
-            .joinToString("")
-        val summand2 = device.input
-            .filter { it.key.startsWith("y") }
-            .map { if (it.value) "1" else "0" }
-            .reversed()
-            .joinToString("")
-
-        check(summand1.toLong(2) < Long.MAX_VALUE / 2)
-        check(summand2.toLong(2) < Long.MAX_VALUE / 2)
-
-        val expectedResult = (summand1.toLong(2) + summand2.toLong(2)).toString(2)
-        return expectedResult
+        return "-"
     }
 
     private fun readInput(input: List<String>): Device {
@@ -111,9 +87,9 @@ class Day24 : AbstractDailyPuzzle() {
             val operand1 = solveForValueOnWire(target.first)
             val operand2 = solveForValueOnWire(target.second)
             val result = when (target.third) {
-                Operator.OR -> operand1 || operand2
-                Operator.AND -> operand1 && operand2
-                Operator.XOR -> operand1 != operand2
+                OR -> operand1 || operand2
+                AND -> operand1 && operand2
+                XOR -> operand1 != operand2
             }
             knownValues[wire] = result
             return result
